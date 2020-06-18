e2a15c9369be4708f371fb6b4b9c2d9cb79a7b90
HMR-7 <51979372+HMR-7@users.noreply.github.com>
2020-06-18 10:56:34 +0800

    基于Promise+async、awaitd的ajax方法

--- await.js
+++ await.js
@@ -2,15 +2,13 @@
 ajax: async function (url, method, data, res) {
     const promise = new Promise((resolve, reject) => {
         if (true) {
-            data.is_mobile = 0
-            data.device_info = 'wxapp'
             // let userInfo = uni.getStorageSync('storaguserInfoe_key');
-            /* if (!data) data = {};
+            if (!data) data = {};
             if (data) {
                 data.user_id = userInfo.id
                 data.sign = userInfo.sign
                 data.openid = userInfo.openid
-            } */
+            }
             console.log(url + '-->' + '请求数据data--->', data)
             uni.request({
                 url: api.config.url + url, //仅为示例，并非真实接口地址。
@@ -25,8 +23,8 @@ ajax: async function (url, method, data, res) {
                     let result = res.data,
                         code = result.code,
                         msg = result.msg;
-                    // console.log(url + '-->' + '请求接口返回值--->', res)
-                    // console.log(url + '-->' + "接口code码返回值--->", code);
+                    console.log(url + '-->' + '请求接口返回值--->', res)
+                    console.log(url + '-->' + "接口code码返回值--->", code);
                     if (code == 40001 || code == 0) {
                         utils.showToast(msg, false)
                         return
